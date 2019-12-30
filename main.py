1e003c1ee096d81b13b75790c51092ca1b8b7aa3
Max1Manley <37525559+Max1Manley@users.noreply.github.com>
2019-12-29 21:52:48 -0600

    adding initial files

---
+++ main.py
@@ -0,0 +1,11 @@
+meep = input("what are you looking for?")
+zoo = ["elephant", "tiger", "lion", "bear"]
+count = 0
+for i in zoo:
+    if meep == i:
+        print("wow. very cool, I found a", i)
+    else:
+        count += 1
+    print(count)
+    if count >= len(zoo):
+        print('no matches found')
---
+++ side.py
@@ -0,0 +1,11 @@
+numbers = [1,2,3,4,5,6,11.23]
+
+for i in numbers:
+    if i%2 == 0:
+        print(i)
+    elif i%1 != 0:
+        print("what the heck kinda garbage are you throwing at me?", i)
+    else:
+        print("uneven number", i)
+        
+#and is pythons version of JSs && operator!
