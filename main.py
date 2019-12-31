d10dc2562a9d814b3e4c9bbbe3940af8a54d7822
max1manley <max1manley@gmail.com>
2019-12-30 22:41:56 -0600

    jumping/comments

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+screenWidth = 640
+screenHeight = 480
+
+win = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Super Python World")
 
+#character dimentions and velocity
 x= 250
 y = 250
 width = 20
 height = 30
 vel = 5
 
+isJump = False
+jumpCount = 10
+
 run = True
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -20,18 +27,31 @@ while run:
             
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > vel:
         x -= vel
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < screenWidth - width - vel:
         x += vel
-    if keys[pygame.K_UP]:
-        y -= vel
-    if keys[pygame.K_DOWN]:
-        y += vel
+    if not(isJump):
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < screenHeight - height - vel:
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
         
     win.fill((0,0,0))
     pygame.draw.rect(win, (0, 0, 255), (x, y, width, height))
-    pygame.draw.circle(win, (255, 0, 255), (50, 50), 30)
+    #pygame.draw.circle(win, (255, 0, 255), (50, 50), 30)
     pygame.display.update()
             
 pygame.quit()
