8683477177346f46eb54c02d93a387c8f585864d
max1manley <max1manley@gmail.com>
2019-12-30 21:47:41 -0600

    sending pygame file

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-meep = input("what are you looking for?")
-zoo = ["elephant", "tiger", "lion", "bear"]
-count = 0
-for i in zoo:
-    if meep == i:
-        print("wow. very cool, I found a", i)
-    else:
-        count += 1
-    print(count)
-    if count >= len(zoo):
-        print('no matches found')
-#just seeing if i can cause some problems!
-        #adding more comments for practice.
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Super Python World")
+
+x= 250
+y = 250
+width = 20
+height = 30
+vel = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT]:
+        x -= vel
+    if keys[pygame.K_RIGHT]:
+        x += vel
+    if keys[pygame.K_UP]:
+        y -= vel
+    if keys[pygame.K_DOWN]:
+        y += vel
+        
+    win.fill((0,0,0))
+    pygame.draw.rect(win, (0, 0, 255), (x, y, width, height))
+    pygame.draw.circle(win, (255, 0, 255), (50, 50), 30)
+    pygame.display.update()
+            
+pygame.quit()
+    
