d2ef73b0b57c224b63a69daaa89f73615b9f3579
max1manley <max1manley@gmail.com>
2019-12-31 22:02:55 -0600

    sprite capabilities

--- main.py
+++ main.py
@@ -7,19 +7,38 @@ screenHeight = 480
 win = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Super Python World")
 
+#used to help set image background
+#bg = pygame.image.load('bg.jpg')
+
+#used to help set framerate
+clock = pygame.time.Clock()
+
 #character dimentions and velocity
 x= 250
 y = 250
 width = 20
 height = 30
 vel = 5
-
 isJump = False
 jumpCount = 10
+left = False
+right = False
+walkCount = 0
+
+#this is a function in python
+def redrawGameWindow():
+    global walkCount
+    win.fill((0,0,0))
+    #image background
+    #win.blit(bg, (-100, -100))
+    pygame.draw.rect(win, (0, 0, 255), (x, y, width, height))
+    #pygame.draw.circle(win, (255, 0, 255), (50, 50), 30)
+    pygame.display.update()
 
 run = True
 while run:
-    pygame.time.delay(50)
+    #frame rate 27/s
+    clock.tick(27)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -27,6 +46,7 @@ while run:
             
     keys = pygame.key.get_pressed()
     
+    #movement listeners
     if keys[pygame.K_LEFT] and x > vel:
         x -= vel
     if keys[pygame.K_RIGHT] and x < screenWidth - width - vel:
@@ -48,11 +68,8 @@ while run:
         else:
             isJump = False
             jumpCount = 10
-        
-    win.fill((0,0,0))
-    pygame.draw.rect(win, (0, 0, 255), (x, y, width, height))
-    #pygame.draw.circle(win, (255, 0, 255), (50, 50), 30)
-    pygame.display.update()
             
+    redrawGameWindow()
+    
 pygame.quit()
     
