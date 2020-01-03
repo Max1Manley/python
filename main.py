abf1a2bfe119d801c0146e119f9debd901c00541
max1manley <max1manley@gmail.com>
2020-01-03 09:41:52 -0600

    more playing with shapes

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ pygame.display.set_caption("Super Python World")
 #used to help set framerate
 clock = pygame.time.Clock()
 
+
+class player(object):
 #character dimentions and velocity
-x= 250
-y = 250
-width = 20
-height = 30
-vel = 5
-isJump = False
-jumpCount = 10
-left = False
-right = False
-walkCount = 0
+    def __init__(self, x, y, width, height):
+        self.x= x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.jumpCount = 10
+        self.left = False
+        self.right = False
+        self.walkCount = 0
 charColor = 255, 0, 255
 white = 255, 255, 255
 purple = 255, 0, 255
@@ -34,10 +37,15 @@ def redrawGameWindow():
     win.fill((0,0,0))
     #image background
     #win.blit(bg, (-100, -100))
-    pygame.draw.rect(win, (charColor), (x, y, width, height))
+    pygame.draw.rect(win, (charColor), (man.x, man.y, man.width, man.height))
     pygame.draw.circle(win, (255, 0, 255), (190, 190), 50, 5)
     pygame.draw.circle(win, (255, 0, 255), (190, 190), 30, 5)
-    pygame.draw.polygon(win, (100, 200, 200), ((0,0), (130, 0), (130, 130), (75, 200), (55, 75)))
+    pygame.draw.polygon(win,
+        (100, 200, 200),
+        ((0,0), (130, 0), (130, 130), (75, 200), (55, 75)))
+    pygame.draw.polygon(win,
+        (100, 200, 200),
+        ((150,0), (280, 0), (280, 130), (225, 200), (205, 75)))
     pygame.display.update()
 
 def handleColor():
@@ -46,7 +54,8 @@ def handleColor():
         if charColor == purple:
             charColor = white
             print('if purple')
-
+            
+man = player(250, 250, 20, 30)
 run = True
 while run:
     #frame rate 27/s
@@ -59,14 +68,14 @@ while run:
     keys = pygame.key.get_pressed()
     
     #movement listeners
-    if keys[pygame.K_a] and x > vel:
-        x -= vel
-    if keys[pygame.K_d] and x < screenWidth - width - vel:
-        x += vel
-    if keys[pygame.K_w] and y > vel:
-        y -= vel
-    if keys[pygame.K_s] and y < screenHeight - height - vel:
-        y += vel
+    if keys[pygame.K_a] and man.x > man.vel:
+        man.x -= man.vel
+    if keys[pygame.K_d] and man.x < screenWidth - man.width - man.vel:
+        man.x += man.vel
+    if keys[pygame.K_w] and man.y > man.vel:
+        man.y -= man.vel
+    if keys[pygame.K_s] and man.y < screenHeight - man.height - man.vel:
+        man.y += man.vel
     handleColor()
             
     redrawGameWindow()
