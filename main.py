9abedd31f15eb9b47d220d3125f8d0a0fd0de825
max1manley <max1manley@gmail.com>
2020-01-04 16:56:51 -0600

    classes added

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+#IDEAS
+#hexagonal dungeon layout
+#items/pickups/areas that provide new elemental powers and give advantages over certain enemies
+#fairy(pink) > dragon(gold) > light(white) > void(black) > psychic(purple) > fairy(pink) 
+#turn based? time based? real time?
+#high ground advantage
+
 import pygame
 pygame.init()
 
-screenWidth = 640
-screenHeight = 480
+#some good old global variable for you
+screenWidth = 1080
+screenHeight = 720
+fairy = 255, 102, 204
+dragon = 204, 153, 0
+light = 255, 255, 255
+void = 115, 115, 115
+psychic = 204, 0, 204
 
+#setting window size and window title
 win = pygame.display.set_mode((screenWidth, screenHeight))
-pygame.display.set_caption("Super Python World")
+pygame.display.set_caption("Element Dungeon")
 
 #used to help set image background
 #bg = pygame.image.load('bg.jpg')
@@ -13,50 +27,77 @@ pygame.display.set_caption("Super Python World")
 #used to help set framerate
 clock = pygame.time.Clock()
 
-
-class player(object):
+#player class
+class player:
 #character dimentions and velocity
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height, charElement):
         self.x= x
         self.y = y
         self.width = width
         self.height = height
+        self.charElement = charElement
         self.vel = 5
-        self.isJump = False
-        self.jumpCount = 10
         self.left = False
         self.right = False
-        self.walkCount = 0
-charColor = 255, 0, 255
-white = 255, 255, 255
-purple = 255, 0, 255
+#bad buy class
+class baddie:
+    def __init__(self, x, y, width, height, charElement, movement, attack, hp):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.charElement = charElement
+        self.vel = 3
+        self.left = False
+        self.right = False
+        self.movement = movement
+        self.attack = attack
+        self.hp = hp
 
-#this is a function in python
+#redraws game window so there isn't a million of the same image, order matters
 def redrawGameWindow():
-    global walkCount
     win.fill((0,0,0))
     #image background
     #win.blit(bg, (-100, -100))
-    pygame.draw.rect(win, (charColor), (man.x, man.y, man.width, man.height))
-    pygame.draw.circle(win, (255, 0, 255), (190, 190), 50, 5)
-    pygame.draw.circle(win, (255, 0, 255), (190, 190), 30, 5)
-    pygame.draw.polygon(win,
-        (100, 200, 200),
-        ((0,0), (130, 0), (130, 130), (75, 200), (55, 75)))
-    pygame.draw.polygon(win,
-        (100, 200, 200),
-        ((150,0), (280, 0), (280, 130), (225, 200), (205, 75)))
+    pygame.draw.polygon(win,(100, 200, 200), ((50,100), (75, 75), (125, 75), (150, 100), (150,150), (125, 175), (75, 175), (50, 150)))
+    pygame.draw.rect(win, (man.charElement), (man.x, man.y, man.width, man.height))
+    #example of drawing a circle and a polygon
+    #pygame.draw.circle(win, (255, 0, 255), (190, 190), 30, 5)
     pygame.display.update()
 
-def handleColor():
-    global charColor
-    if keys[pygame.K_SPACE]:
-        if charColor == purple:
-            charColor = white
-            print('if purple')
-            
-man = player(250, 250, 20, 30)
+#function for changing player state
+def handleElement():
+    #changing colors with keys
+    if keys[pygame.K_1]:
+        man.charElement = fairy
+        print('making fairy')
+    if keys[pygame.K_2]:
+        man.charElement = dragon
+        print('making dragon')
+    if keys[pygame.K_3]:
+        man.charElement = light
+        print('making light')
+    if keys[pygame.K_4]:
+        man.charElement = void
+        print('making void')
+    if keys[pygame.K_5]:
+        man.charElement = psychic
+        print('making psychic')
+        
+    #playing with mouse events
+    if mouse[0]:
+        print('left click', event.pos)
+    if mouse[1]:
+        print('middle click', mouse)
+    if mouse[2]:
+        print('right click', event)
+
+#creating character
+man = player(500, 500, 20, 30, light)
+bad = baddie(300, 300, 30, 20, void, 3, 5, 15)
+
 run = True
+#main game loop
 while run:
     #frame rate 27/s
     clock.tick(27)
@@ -64,8 +105,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-            
+    #listeners for mouse and key presses        
     keys = pygame.key.get_pressed()
+    mouse = pygame.mouse.get_pressed()
     
     #movement listeners
     if keys[pygame.K_a] and man.x > man.vel:
@@ -76,11 +118,18 @@ while run:
         man.y -= man.vel
     if keys[pygame.K_s] and man.y < screenHeight - man.height - man.vel:
         man.y += man.vel
-    handleColor()
-            
+        
+    #bad guy movement
+    if keys[pygame.K_a] and bad.x > bad.vel:
+        bad.x -= bad.vel
+    if keys[pygame.K_d] and bad.x < screenWidth - bad.width - bad.vel:
+        bad.x += bad.vel
+    if keys[pygame.K_w] and bad.y > bad.vel:
+        bad.y -= bad.vel
+    if keys[pygame.K_s] and bad.y < screenHeight - bad.height - bad.vel:
+        bad.y += bad.vel
+    
+    handleElement()
     redrawGameWindow()
     
-
-    
 pygame.quit()
-    
